3cda07aed527b70ebc1063fb39caf9a766c7aba3
MoonBear-bit <dytpql03@naver.com>
2025-05-11 16:08:25 +0900

    Add files via upload

--- Code/Script.js
+++ Code/Script.js
@@ -4,34 +4,78 @@ let egg=document.getElementById('egg');
 let grass=document.getElementById('world');
 let golder=document.getElementById('money');
 let tree_num=document.getElementById('tree_num');
+let rock_num=document.getElementById('rock_num');
+let alerts=document.getElementById('alert');
+let yes=document.getElementById('yes');
+let no=document.getElementById('no');
+let taketree=document.getElementById('taketree')
+let iftaketreebutton=document.getElementById('iftaketreebutton')
 egg.style.display='none'
 let gold=0;
 let tree=0;
+let rock=0;
 let opening=new Audio("Sound/North's panda.mp3");
 let goldsound=new Audio('Sound/gold.mp3')
 let eggsound=new Audio('Sound/egg.mp3')
-let treesound=new Audio('Sound/bomb.mp3')
+let treesound=new Audio('Sound/tree.mp3')
+let rocksound=new Audio('Sound/rock.mp3')
+let start=new Audio('Sound/system start.mp3')
 let ifstart=false;
 let money=0;
 let all=[];
-let golds=[]
-let trees=[]
+let golds=[];
+let trees=[];
+let rocks=[];
 let entity=['Northpolebear','Firebear','Whatgo','Fiveridecoo','Twistworm','Realdotdog','Blackpig','Silverlongfish'];
 let entity_all={
     Northpolebear_all:{power:3,speed:2,get:1,create:1},
     Firebear_all:{power:3,speed:2,get:1,create:1},
     Whatgo_all:{power:1,speed:2,get:1,create:3},
-    Fiveridecoo_all:{power:1,speed:2.5,get:1,create:1},
+    Fiveridecoo_all:{power:1,speed:3,get:1,create:1},
     Twistworm_all:{power:2,speed:2,get:2,create:2},
     Realdotdog_all:{power:1,speed:2,get:3,create:1},
-    Blackpig_all:{power:1,speed:2.5,get:1,create:1},
+    Blackpig_all:{power:1,speed:3,get:1,create:1},
     Silverlongfish_all:{power:1,speed:2,get:1,create:3},
 }
 let follow=[];
-popki.style.display='none'
-golder.style.display='none'
-tree_num.style.display='none'
+let name=''
+popki.style.display='none';
+golder.style.display='none';
+tree_num.style.display='none';
+rock_num.style.display='none';
+alerts.style.display='none';
+yes.style.display='none';
+no.style.display='none';
+taketree.style.display='none';
+iftaketreebutton.style.display='none';
 let need_money=null;
+function starttutorial(){
+    alert('화면에는 지렁이가 보일겁니다.');
+    alert('지렁이를 클릭하면 움직입니다. 멈추려면 다시 클릭하세요.')
+    alert('우측 상단의 사각형들은 자원의 수를 나타냅니다.')
+    alert('자원은 가까이서 채집합니다.')
+    alert('금을 모아 알로 새 일꾼을 고용하세요.')
+    alert('자, 이제, 당신의 시간입니다. 건설하고, 싸우세요.')
+}
+yes.addEventListener('click', (()=>{
+    starttutorial();
+    alerts.style.display='none';
+    yes.style.display='none';
+    no.style.display='none';
+}));
+no.addEventListener('click', ()=>{
+    alerts.style.display='none';
+    yes.style.display='none';
+    no.style.display='none';
+});
+function what_(name){
+    alerts.style.display='block';
+    yes.style.display='block';
+    no.style.display='block';
+    if (name=='튜토리얼'){
+        alerts.textContent='Tutorial?'; 
+    }
+}
 function update(){
     if (ifstart){
         StartButton.style.display="none";
@@ -39,17 +83,30 @@ function update(){
         popki.style.display='block'
         golder.style.display='block'
         tree_num.style.display='block'
+        rock_num.style.display='block'
+        iftaketreebutton.style.display='block';
+        //taketree.style.display='block';
         gold_make();
         tree_make();
+        rock_make();
     }else{
         StartButton.style.display="block";
     }
 }
 update();
+iftaketreebutton.addEventListener('click',()=>{
+    if (taketree.style.display=='block'){
+        taketree.style.display='none'
+    }else{
+        taketree.style.display='block'
+    }
+});
 StartButton.addEventListener('click',()=>{
     if (ifstart){
         ifstart=false;
     }else{
+        let name='튜토리얼'
+        what_(name);
         ifstart=true;
     }
     update();
@@ -80,20 +137,6 @@ function __random__(){
     }
     return reterner;
 }   
-function create_egg(y,x){
-    const egg_start=document.createElement("div");
-    egg_start.className="egg";
-    egg_start.style.position="absolute";
-    egg_start.style.top=y;
-    egg_start.style.left=x;
-    egg_start.style.opacity=1;
-    egg_start.src="/Image/egg.png";
-    document.body.appendChild(egg_start);
-    egg_start.addEventListener('click',()=>{
-        eggsound.play();
-        egg_start.remove();
-    });
-}
 function create(wt){
     const creater=document.createElement("button");
     creater.id=wt;
@@ -104,7 +147,6 @@ function create(wt){
     creater.style.left=String(Math.floor(Math.random()*(window.innerWidth-51)))+'px';
     document.body.appendChild(creater);
     all.push(creater);
-    create_egg(creater.style.top,creater.style.left);
     follow=[]
     for (var i=0; i<all.length; i++){
         follow.push(false);
@@ -113,55 +155,90 @@ function create(wt){
     forfor();
 }
 function create_gold(){
-    let xrandom=Math.floor(Math.random()*(window.innerWidth-51));
-    let yrandom=Math.floor(Math.random()*(window.innerHeight-51));
-    xrandom=String(xrandom)+'px';
-    yrandom=String(yrandom)+'px';
-    let creategold=document.createElement('button');
-    creategold.className='gold';
-    creategold.type='button';
-    creategold.style.left=xrandom;
-    creategold.style.top=yrandom;
-    creategold.style.display='block';
-    creategold.id='gold';
-    document.body.appendChild(creategold);
-    golds.push(creategold);
-    golds[golds.length-1].addEventListener('click',()=>{
-        for (var i=0;i<all.length;i++){
-            if (math(all[i],creategold)<150){
-                goldsound.play();
-                creategold.remove();
-                gold+=1;
-                return
+    if (gold<20){
+        let xrandom=Math.floor(Math.random()*(window.innerWidth-51));
+        let yrandom=Math.floor(Math.random()*(window.innerHeight-51));
+        xrandom=String(xrandom)+'px';
+        yrandom=String(yrandom)+'px';
+        let creategold=document.createElement('button');
+        creategold.className='gold';
+        creategold.type='button';
+        creategold.style.left=xrandom;
+        creategold.style.top=yrandom;
+        creategold.style.display='block';
+        creategold.id='gold';
+        document.body.appendChild(creategold);
+        golds.push(creategold);
+        golds[golds.length-1].addEventListener('click',()=>{
+            for (var i=0;i<all.length;i++){
+                if (math(all[i],creategold)<150){
+                    goldsound.play();
+                    creategold.remove();
+                    gold+=1;
+                    return
+                }
             }
-        }
-    });
+        });
+    }
+    
 }
 function create_tree(){
-    let xrandom=Math.floor(Math.random()*(window.innerWidth-51));
-    let yrandom=Math.floor(Math.random()*(window.innerHeight-51));
-    xrandom=String(xrandom)+'px';
-    yrandom=String(yrandom)+'px';
-    let createtree=document.createElement('button');
-    createtree.className='tree';
-    createtree.type='button';
-    createtree.style.left=xrandom;
-    createtree.style.top=yrandom;
-    createtree.style.display='block';
-    createtree.id='tree';
-    document.body.appendChild(createtree);
-    trees.push(createtree);
-    trees[trees.length-1].addEventListener('click',()=>{
-        for (var i=0;i<all.length;i++){
-            if (math(all[i],createtree)<150){
-                treesound.play();
-                createtree.remove();
-                tree+=1;
-                return
+    if (tree<10){
+        let xrandom=Math.floor(Math.random()*(window.innerWidth-51));
+        let yrandom=Math.floor(Math.random()*(window.innerHeight-51));
+        xrandom=String(xrandom)+'px';
+        yrandom=String(yrandom)+'px';
+        let createtree=document.createElement('button');
+        createtree.className='tree';
+        createtree.type='button';
+        createtree.style.left=xrandom;
+        createtree.style.top=yrandom;
+        createtree.style.display='block';
+        createtree.id='tree';
+        document.body.appendChild(createtree);
+        trees.push(createtree);
+        trees[trees.length-1].addEventListener('click',()=>{
+            for (var i=0;i<all.length;i++){
+                if (math(all[i],createtree)<150){
+                    treesound.play();
+                    createtree.remove();
+                    tree+=1;
+                    return
+                }
             }
-        }
-        
-    });
+            
+        });
+    }
+    
+}
+function create_rock(){
+    if (rock<10){
+        let xrandom=Math.floor(Math.random()*(window.innerWidth-51));
+        let yrandom=Math.floor(Math.random()*(window.innerHeight-51));
+        xrandom=String(xrandom)+'px';
+        yrandom=String(yrandom)+'px';
+        let createrock=document.createElement('button');
+        createrock.className='rock';
+        createrock.type='button';
+        createrock.style.left=xrandom;
+        createrock.style.top=yrandom;
+        createrock.style.display='block';
+        createrock.id='rock';
+        document.body.appendChild(createrock);
+        rocks.push(createrock);
+        rocks[rocks.length-1].addEventListener('click',()=>{
+            for (var i=0;i<all.length;i++){
+                if (math(all[i],createrock)<150){
+                    rocksound.play();
+                    createrock.remove();
+                    rock+=1;
+                    return
+                }
+            }
+            
+        });  
+    }
+    
 }
 function math(elem1,elem2){
     const x1 = elem1.offsetLeft;
@@ -204,25 +281,45 @@ popki.addEventListener('mouseenter',()=>{
 popki.addEventListener('mouseleave',()=>{
     popki.style.opacity=1;
 });
+golder.addEventListener('mouseenter',()=>{
+    golder.style.opacity=0.5;
+});
+golder.addEventListener('mouseleave',()=>{
+    golder.style.opacity=1;
+});
+tree_num.addEventListener('mouseenter',()=>{
+    tree_num.style.opacity=0.5;
+});
+tree_num.addEventListener('mouseleave',()=>{
+    tree_num.style.opacity=1;
+});
+rock_num.addEventListener('mouseenter',()=>{
+    rock_num.style.opacity=0.5;
+});
+rock_num.addEventListener('mouseleave',()=>{
+    rock_num.style.opacity=1;
+});
 document.addEventListener("mousemove",(event)=>{
     x=event.clientX;
     y=event.clientY;
 });
 function gold_make(){
-    if (ifstart){
-        setTimeout(()=>{
-            create_gold();
-            gold_make();
-        },Math.floor(5000+(Math.random()*(10000-5000))))
-    }
+    setTimeout(()=>{
+        create_gold();
+        gold_make();
+    },Math.floor(5000+(Math.random()*(10000-5000))))
 }
 function tree_make(){
-    if (ifstart){
-        setTimeout(()=>{
-            create_tree();
-            tree_make();
-        },Math.floor(5000+(Math.random()*(10000-5000))))
-    }
+    setTimeout(()=>{
+        create_tree();
+        tree_make();
+    },Math.floor(5000+(Math.random()*(10000-5000))))
+}
+function rock_make(){
+    setTimeout(()=>{
+        create_rock();
+        rock_make();
+    },Math.floor(5000+(Math.random()*(10000-5000))))
 }
 popki.addEventListener('click',()=>{
     if (gold>=need_money){
@@ -233,18 +330,23 @@ popki.addEventListener('click',()=>{
 function move(i,xx,yy,n){
     if (follow[i]){
         if (xx>0){
-            all[i].style.left=String(parseInt(all[i].style.left.replace('px',''))+Math.random()*n)+"px";
-        }else if (xx<0){
-            all[i].style.left=String(parseInt(all[i].style.left.replace('px',''))-Math.random()*n)+"px";
+            all[i].style.left=String(parseInt(all[i].style.left.replace('px',''))+(n-(n/3)))+"px";
+        }else if (xx<=0){
+            all[i].style.left=String(parseInt(all[i].style.left.replace('px',''))-(n-(n/3)))+"px";
         }
         if (yy>0){
-            all[i].style.top=String(parseInt(all[i].style.top.replace('px',''))+Math.random()*n)+"px";
-        }else if (yy<0){
-            all[i].style.top=String(parseInt(all[i].style.top.replace('px',''))-Math.random()*n)+"px";
+            all[i].style.top=String(parseInt(all[i].style.top.replace('px',''))+(n-(n/3)))+"px";
+        }else if (yy<=0){
+            all[i].style.top=String(parseInt(all[i].style.top.replace('px',''))-(n-(n/3)))+"px";
         }
     }
 }
 function yee(){
+    if (taketree.style.display=='block'){
+        iftaketreebutton.style.left='50vw'
+    }else{
+        iftaketreebutton.style.left='0vw'
+    }
     if (follow.includes(true)){
         grass.style.cursor='crosshair';
     }else{
@@ -254,6 +356,7 @@ function yee(){
     popki.textContent=String(need_money);
     golder.textContent=String(gold);
     tree_num.textContent=String(tree);
+    rock_num.textContent=String(rock);
     for (var i=0; i<all.length; i++){
         var l=parseInt(all[i].style.left.replace('px',''))+35;
         var t=parseInt(all[i].style.top.replace('px',''))+35;
